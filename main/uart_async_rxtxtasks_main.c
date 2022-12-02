819a788cd67edf9a9053f3abfcf94f7eb2dfdb59
David <dav.gruber@htlstp.at>
2022-12-03 00:18:09 +0100

    add: Queue test

--- main/uart_async_rxtxtasks_main.c
+++ main/uart_async_rxtxtasks_main.c
@@ -1,5 +1,6 @@
 #include <sys/cdefs.h>
 #include <mbedtls/aes.h>
+
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_system.h"
@@ -14,6 +15,9 @@
 static const int RX_BUF_SIZE = 1024;
 uint8_t plaintext[16384];
 uint8_t encrypted[16384];
+
+QueueHandle_t messageQueue;
+QueueHandle_t encryptedQueue;
 
 int sendData(const char *logName, const char *data);
 
@@ -72,7 +76,7 @@ _Noreturn static void tx_task(void *arg) {
     static const char *TX_TASK_TAG = "TX_TASK";
     esp_log_level_set(TX_TASK_TAG, ESP_LOG_INFO);
     while (1) {
-        sendData(TX_TASK_TAG, "Hello, World!");
+        sendData(TX_TASK_TAG, "Hello, World!\n");
         vTaskDelay(2000 / portTICK_PERIOD_MS);
     }
 }
@@ -92,6 +96,11 @@ static void rx_task(void *arg) {
     free(data);
 }
 
+static void encrypt_task(void *arg) {
+    static const char *EN_TASK_TAG = "ENC_TASK";
+    esp_log_level_set(EN_TASK_TAG, ESP_LOG_INFO);
+}
+
 
 static inline int32_t getCycleCount(void) {
     int32_t ccount;
@@ -102,25 +111,22 @@ static inline int32_t getCycleCount(void) {
 double avgenc = 0;
 double avgdec = 0;
 
-void aes_generate_key(uint8_t *key, int length) {
-    esp_fill_random(key, length);
-}
 
-
-int encodetest() {
+int encodetest(int buff_len) {
     const int key_length = 256;
-    const int blocksize = 16;
     int val[2] = {10000, 1000};
+    const char* text = "Someserious stuff";
     uint8_t key[key_length];
-    uint8_t iv[blocksize];
+    uint8_t iv[16];
 
     //If you have cryptographically random data in the start of your payload, you do not need
     //an IV.  If you start a plaintext payload, you will need an IV.
-    memset(iv, 2, sizeof(iv));
-    esp_fill_random(key, sizeof(key));
+    memset(iv, 61, sizeof(iv));
+    memset(key, 61, sizeof(key));
+    //esp_fill_random(key, sizeof(key));
 
-    memset(plaintext, 0, sizeof(plaintext));
-    memcpy(plaintext, &val, sizeof(val));
+    memset(plaintext, 0, buff_len);
+    strcpy((char *)plaintext, text);
 
     //Just FYI - you must be encrypting/decrypting data that is in BLOCKSIZE chunks!!!
 
@@ -129,32 +135,35 @@ int encodetest() {
     if (esp_aes_setkey(&ctx, key, key_length) == MBEDTLS_ERR_AES_INVALID_KEY_LENGTH) {
         ESP_LOGE("AES_CONFIG", "Invalid Keylength");
     }
-    ESP_LOG_BUFFER_HEXDUMP("AES_DATA", plaintext, sizeof(val), ESP_LOG_INFO);
+    ESP_LOG_BUFFER_HEXDUMP("AES_DATA", plaintext, strlen(text), ESP_LOG_INFO);
+    ESP_LOG_BUFFER_HEXDUMP("AES_IV", iv, 16, ESP_LOG_INFO);
     ESP_LOG_BUFFER_HEXDUMP("AES_KEY", key, key_length, ESP_LOG_INFO);
     int32_t start = getCycleCount();
-    esp_aes_crypt_cbc(&ctx, ESP_AES_ENCRYPT, sizeof(plaintext), iv, plaintext, encrypted);
+    esp_aes_crypt_cbc(&ctx, ESP_AES_ENCRYPT, buff_len, iv, plaintext, encrypted);
     int32_t end = getCycleCount();
     double enctime = (end - start) / 240.0;
     avgenc += enctime;
-    printf("Encryption time: %.2fus  (%f MB/s)\n", enctime, (sizeof(plaintext) * 1.0) / enctime);
+    printf("Encryption time: %.2fus  (%f MB/s)\n", enctime, buff_len * 1.0 / enctime);
     //See encrypted payload, and wipe out plaintext.
-    memset(plaintext, 0, sizeof(plaintext));
-    for (int i = 0; i < 128; i++) {
-        printf("%02x[%c]%c", encrypted[i], (encrypted[i] > 31) ? encrypted[i] : ' ', ((i & 0xf) != 0xf) ? ' ' : '\n');
-    }
-    printf("\n");
-    //printf( "IV: %02x %02x\n", iv[0], iv[1] );
-    memset(iv, 2, sizeof(iv));
-    memset(&plaintext, 0, sizeof (plaintext));
+    ESP_LOG_BUFFER_HEXDUMP("AES_ENCRYPTED", encrypted, buff_len, ESP_LOG_INFO);
+    printf("%s", encrypted);
+    memset(plaintext, 0, buff_len);
+    //for (int i = 0; i < 128; i++) {
+    //    printf("%02x[%c]%c", encrypted[i], (encrypted[i] > 31) ? encrypted[i] : ' ', ((i & 0xf) != 0xf) ? ' ' : '\n');
+    //}
+    //printf("\n");
+    printf( "IV: %02x %02x\n", iv[0], iv[1] );
+    memset(iv, 61, sizeof(iv));
+    memset(&plaintext, 0, buff_len);
     memset(&val, 0, sizeof (val));
 
     //Use the ESP32 to decrypt the CBC block.
     start = getCycleCount();
-    esp_aes_crypt_cbc(&ctx, ESP_AES_DECRYPT, sizeof(encrypted), iv, (uint8_t *) encrypted, (uint8_t *) plaintext);
+    esp_aes_crypt_cbc(&ctx, ESP_AES_DECRYPT, buff_len, iv, (uint8_t *) encrypted, (uint8_t *) plaintext);
     end = getCycleCount();
     enctime = (end - start) / 240.0;
     avgdec += enctime;
-    printf("Decryption time: %.2fus  (%f MB/s)\n", enctime, (sizeof(plaintext) * 1.0) / enctime);
+    printf("Decryption time: %.2fus  (%f MB/s)\n", enctime, buff_len * 1.0 / enctime);
 
     //Verify output
     for (int i = 0; i < 128; i++) {
@@ -162,8 +171,8 @@ int encodetest() {
     }
     printf("\n");
     memcpy(&val, plaintext, sizeof (val));
-    ESP_LOG_BUFFER_HEXDUMP("PLAIN-INT",&val, sizeof (val), ESP_LOG_INFO);
-    printf("%d %d\n", val[0], val[1]);
+    //ESP_LOG_BUFFER_HEXDUMP("PLAIN-INT",&val, sizeof (val), ESP_LOG_INFO);
+    //printf("%d %d\n", val[0], val[1]);
 
     esp_aes_free(&ctx);
     return 0;
@@ -172,9 +181,36 @@ int encodetest() {
 
 
 void app_main(void) {
-    encodetest();
+    //int buff_lens_to_test[] = {256, 512, 1024, 2048, 4096, 8192,16384};
+    //for(int i = 0;i<sizeof(buff_lens_to_test)/sizeof(buff_lens_to_test[0]);i++) {
+    //    printf("Buffer-length-%d:\n", buff_lens_to_test[i]);
+    //    encodetest(buff_lens_to_test[i]);
+    //}
+    int val = 1000;
+    messageQueue = xQueueCreate(10, sizeof(int));
+    encryptedQueue = xQueueCreate(10, sizeof(int));
+    for(int i = 0; i < 10; i++) {
+        xQueueSendToBack(messageQueue, &val, 0);
+    }
+    if(uxQueueMessagesWaiting(messageQueue)) {
+        ESP_LOGI("QUEUE", "Messages in Queue");
+    }
+    int *plain = malloc(sizeof(int)*10);
+    memset(plain, 0, 10 * sizeof(int));
+    for(int i = 0; i < 10; i++) {
+        xQueueReceive(messageQueue, plain+i, 0);
+    }
+    ESP_LOG_BUFFER_HEXDUMP("QUEUE", plain, sizeof(int)*10, ESP_LOG_INFO);
+    if(val == plain[0])
+        ESP_LOGI("QUEUE", "success");
 
-    //init();
-    //xTaskCreate(rx_task, "uart_rx_task", 1024*2, NULL, configMAX_PRIORITIES, NULL);
-    //xTaskCreate(tx_task, "uart_tx_task", 1024*2, NULL, configMAX_PRIORITIES-1, NULL);
+    //encodetest(1024);
+
+    //if(xQueueReceive(messageQueue, plain, 1) == pdTRUE) {
+    //    ESP_LOG_BUFFER_HEXDUMP("ENC-QUEUE", plain, sizeof(plain), ESP_LOG_INFO);
+    //}
+
+    init();
+    xTaskCreate(rx_task, "uart_rx_task", 1024*2, NULL, configMAX_PRIORITIES, NULL);
+    xTaskCreate(tx_task, "uart_tx_task", 1024*2, NULL, configMAX_PRIORITIES-1, NULL);
 }
